b5a4305df44f04e0a4df1bbce7060e7f11ff0b8f
juan2589 <juandiegolondono184@gmail.com>
2025-05-08 17:05:38 -0500

    edicion 2

--- src/compararDosNumeros.java
+++ src/compararDosNumeros.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
-public class compararDosNumeros {
+public class CompararDosNumeros { 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Solicita los dos números
         System.out.print("Ingrese el primer número: ");
-        int num1 = scanner.nextInt();
+        int primerNumero = scanner.nextInt();
 
         System.out.print("Ingrese el segundo número: ");
-        int num2 = scanner.nextInt();
+        int segundoNumero = scanner.nextInt();
 
         // Verifica si el primero es mayor que el segundo
-        if (num1 > num2) {
-            int diferencia = num1 - num2;
+        if (primerNumero > segundoNumero) {
+            int diferencia = primerNumero - segundoNumero;
             System.out.println("El primer número es mayor.");
             System.out.println("La diferencia entre ambos es: " + diferencia);
-        }
-
-        // Verifica si el primero NO es mayor que el segundo
-        if (num1 <= num2) {
+        } else if (primerNumero == segundoNumero) {
+            System.out.println("Ambos números son iguales.");
+        } else {
             System.out.println("El primer número NO es mayor que el segundo.");
         }
 
